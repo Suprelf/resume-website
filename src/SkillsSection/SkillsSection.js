d97a74363e1fe99f06685f0cc6ef8b1957ea38a3
Suprelf <mh.suprelf@gmail.com>
2023-09-25 22:01:02 +0300

    skill and exp overflow

--- src/SkillsSection/SkillsSection.js
+++ src/SkillsSection/SkillsSection.js
@@ -6,8 +6,8 @@ function SkillsSection() {
             <div className="section-flex">
 
                 <div className="card">
-                    <h3 className="card-name">Skills</h3>
-                    <div className="card-main">
+                    <h3 className="card-name" id="card-name-left">Skills</h3>
+                    <div className="card-main" id="skills-scroll">
 
                         <div className="skill-main">
                             <div className="skill-name">
@@ -17,58 +17,27 @@ function SkillsSection() {
                                 <div className="skill-bar-fill"></div>
                             </div>
                         </div>
-
-                        <div className="skill-main">
-                            <div className="skill-name">
-                                JS/HTML/CSS
-                            </div>
-                            <div className="skill-bar">
-                                <div className="skill-bar-fill"></div>
-                            </div>
-                        </div>
-
-                        <div className="skill-main">
-                            <div className="skill-name">
-                                JS/HTML/CSS
-                            </div>
-                            <div className="skill-bar">
-                                <div className="skill-bar-fill"></div>
-                            </div>
-                        </div>                           
-                    
+                        
+                        
                     </div>
                 </div>
 
                 <div className="card borderRight">
-                    <h3>Education and expirience</h3>
-                    <div className="stats-education">
+                    <h3 className="card-name">Education and expirience</h3>
+                    <div className="card-main" id="exp-scroll">
 
-                        <div className="box">
-                            <span className="year">2019 - 2023</span>
-                            <span className="place">
-                                Lviv National University
-                            </span> 
-                            <div className="subject">
+                        <div className="exp-main">
+                            <div className="exp-header">
+                                <div className="exp-year">2019 - 2023</div>
+                                <div className="exp-divider">-</div>
+                                <div className="exp-place">
+                                    Lviv National University
+                                </div>
+                            </div>
+
+                            <div className="exp-info">
                                 Computer Science Bachelor
-                            </div> 
-                        </div>
-                        <div className="box">
-                            <span className="year">2022</span>
-                            <span className="place">
-                                Binariks Angular courses
-                            </span> 
-                            <div className="subject">
-                                Angular and aoao
-                            </div> 
-                        </div>
-                        <div className="box">
-                            <span className="year">2023 - now</span>
-                            <span className="place">
-                                Cherkassy National University
-                            </span> 
-                            <div className="subject">
-                                Inj Prog Zab master
-                            </div> 
+                            </div>
                         </div>
 
 
