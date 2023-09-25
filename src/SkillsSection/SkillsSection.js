2b2f4e19e6a188aa3f848fa7ec05234d53b104a3
Suprelf <mh.suprelf@gmail.com>
2023-09-25 20:41:46 +0300

    skills section layout rework

--- src/SkillsSection/SkillsSection.js
+++ src/SkillsSection/SkillsSection.js
@@ -5,50 +5,42 @@ function SkillsSection() {
         <div className="section">
             <div className="section-flex">
 
-                <div className="stats-left">
-                    <h3>Skills</h3>
-                    <div className="stats-education">
-                        <div className="box">
+                <div className="card">
+                    <h3 className="card-name">Skills</h3>
+                    <div className="card-main">
+
+                        <div className="skill-main">
                             <div className="skill-name">
                                 JS/HTML/CSS
                             </div>
-                            <div className="bar">
-                                <div className="bar-fill"></div>
+                            <div className="skill-bar">
+                                <div className="skill-bar-fill"></div>
                             </div>
                         </div>
 
-                        <div className="box">
-                            <div className="skill-name">
-                                Angular
-                            </div>
-                            <div className="bar">
-                                <div className="bar-fill"></div>
-                            </div>
-                        </div>
-
-                        <div className="box">
+                        <div className="skill-main">
                             <div className="skill-name">
                                 JS/HTML/CSS
                             </div>
-                            <div className="bar">
-                                <div className="bar-fill"></div>
+                            <div className="skill-bar">
+                                <div className="skill-bar-fill"></div>
                             </div>
                         </div>
 
-                        <div className="box">
+                        <div className="skill-main">
                             <div className="skill-name">
                                 JS/HTML/CSS
                             </div>
-                            <div className="bar">
-                                <div className="bar-fill"></div>
+                            <div className="skill-bar">
+                                <div className="skill-bar-fill"></div>
                             </div>
-                        </div>
+                        </div>                           
                     
                     </div>
                 </div>
 
-                <div className="stats-right">
-                    <h3>My education and courses</h3>
+                <div className="card borderRight">
+                    <h3>Education and expirience</h3>
                     <div className="stats-education">
 
                         <div className="box">
