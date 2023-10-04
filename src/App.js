5784aa8fcec8b1c96fcbdb210d947e0afd35ba75
Suprelf <mh.suprelf@gmail.com>
2023-10-05 01:03:32 +0300

    Total rework

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
 import './App.scss';
-import Header from './Header/header';
-import TopSection from './TopSection/TopSection';
-import SkillsSection from './SkillsSection/SkillsSection';
-import Particle from "./Particle/Particle";
 
 function App() {
   return (
     <div className="App">
-        <Particle/>
-
-        <TopSection></TopSection>
-        <SkillsSection></SkillsSection>
-        
-
-
+      hi
     </div>
   );
 }
